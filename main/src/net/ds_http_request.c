b59276578986940be9cf2018e80a120033f0b236
zhifei19 <zhifeiluo@foxmail.com>
2023-11-03 15:30:13 +0800

    Add HTTPS part and tested correctly

--- main/src/net/ds_http_request.c
+++ main/src/net/ds_http_request.c
@@ -36,11 +36,14 @@ static const char *TAG = "ds_HTTP_CLIENT";
    To embed it in the app binary, the PEM file is named
    in the component.mk COMPONENT_EMBED_TXTFILES variable.
 */
-extern const char howsmyssl_com_root_cert_pem_start[] asm("_binary_howsmyssl_com_root_cert_pem_start");
-extern const char howsmyssl_com_root_cert_pem_end[]   asm("_binary_howsmyssl_com_root_cert_pem_end");
+// extern const char howsmyssl_com_root_cert_pem_start[] asm("_binary_howsmyssl_com_root_cert_pem_start");
+// extern const char howsmyssl_com_root_cert_pem_end[]   asm("_binary_howsmyssl_com_root_cert_pem_end");
 
-extern const char postman_root_cert_pem_start[] asm("_binary_postman_root_cert_pem_start");
-extern const char postman_root_cert_pem_end[]   asm("_binary_postman_root_cert_pem_end");
+// extern const char postman_root_cert_pem_start[] asm("_binary_postman_root_cert_pem_start");
+// extern const char postman_root_cert_pem_end[]   asm("_binary_postman_root_cert_pem_end");
+
+extern const char weather_root_cert_pem_start[] asm("_binary_weather_root_cert_pem_start");
+extern const char weather_root_cert_pem_end[]   asm("_binary_weather_root_cert_pem_end");
 
 esp_err_t _http_event_handler(esp_http_client_event_t *evt)
 {
@@ -210,6 +213,90 @@ esp_err_t _http_weather_event_handler(esp_http_client_event_t *evt)
     return ESP_OK;
 }
 
+esp_err_t _https_weather_event_handler(esp_http_client_event_t *evt)
+{
+    static char *output_buffer;  // Buffer to store response of http request from event handler
+    static int output_len;       // Stores number of bytes read
+    switch(evt->event_id) {
+        case HTTP_EVENT_ERROR:
+            ESP_LOGI(TAG, "HTTPS_EVENT_ERROR");
+            break;
+        case HTTP_EVENT_ON_CONNECTED:
+            ESP_LOGI(TAG, "HTTPS_EVENT_ON_CONNECTED");
+            break;
+        case HTTP_EVENT_HEADER_SENT:
+            ESP_LOGI(TAG, "HTTPS_EVENT_HEADER_SENT");
+            break;
+        case HTTP_EVENT_ON_HEADER:
+            ESP_LOGI(TAG, "HTTPS_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            break;
+        case HTTP_EVENT_ON_DATA:
+            ESP_LOGI(TAG, "HTTPS_EVENT_ON_DATA, len=%d", evt->data_len);
+            /*
+             *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+             *  However, event handler can also be used in case chunked encoding is used.
+             */
+            // if (!esp_http_client_is_chunked_response(evt->client)) {
+            //     // If user_data buffer is configured, copy the response into the buffer
+            //     int copy_len = 0;
+            //     if (evt->user_data) {
+            //         copy_len = MIN(evt->data_len, (MAX_HTTP_OUTPUT_BUFFER - output_len));
+            //         if (copy_len) {
+            //             memcpy(evt->user_data + output_len, evt->data, copy_len);
+            //         }
+            //     } else {
+            //         const int buffer_len = esp_http_client_get_content_length(evt->client);
+            //         if (output_buffer == NULL) {
+            //             output_buffer = (char *) malloc(buffer_len);
+            //             output_len = 0;
+            //             if (output_buffer == NULL) {
+            //                 ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+            //                 return ESP_FAIL;
+            //             }
+            //         }
+            //         copy_len = MIN(evt->data_len, (buffer_len - output_len));
+            //         if (copy_len) {
+            //             memcpy(output_buffer + output_len, evt->data, copy_len);
+            //         }
+            //     }
+            //     output_len += copy_len;
+            // }
+
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            ESP_LOGI(TAG, "HTTPS_EVENT_ON_FINISH");
+            if (output_buffer != NULL) {
+                // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+                // ESP_LOG_BUFFER_HEX(TAG, output_buffer, output_len);
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        case HTTP_EVENT_DISCONNECTED:
+            ESP_LOGI(TAG, "HTTPS_EVENT_DISCONNECTED");
+            int mbedtls_err = 0;
+            esp_err_t err = esp_tls_get_and_clear_last_error((esp_tls_error_handle_t)evt->data, &mbedtls_err, NULL);
+            if (err != 0) {
+                ESP_LOGI(TAG, "Last esp error code: 0x%x", err);
+                ESP_LOGI(TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+            }
+            if (output_buffer != NULL) {
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        case HTTP_EVENT_REDIRECT:
+            ESP_LOGI(TAG, "HTTPS_EVENT_REDIRECT");
+            esp_http_client_set_header(evt->client, "From", "user@example.com");
+            esp_http_client_set_header(evt->client, "Accept", "text/html");
+            esp_http_client_set_redirection(evt->client);
+            break;
+    }
+    return ESP_OK;
+}
+
 static void http_get_weather(void)
 {
     char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
@@ -242,6 +329,29 @@ static void http_get_weather(void)
     }
     // ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
 
+    esp_http_client_cleanup(client);
+}
+
+static void https_get_weather(void)
+{
+    esp_http_client_config_t config = {
+        // .host = "www.howsmyssl.com",
+        // .path = "/",
+        .url = "https://api.seniverse.com/v3/weather/now.json?key=SWCiZfZFejLvD9FD3&location=beijing&language=zh-Hans&unit=c",
+        .transport_type = HTTP_TRANSPORT_OVER_SSL,
+        .event_handler = _https_weather_event_handler,
+        .cert_pem = weather_root_cert_pem_start,
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_err_t err = esp_http_client_perform(client);
+
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %"PRIu64,
+                esp_http_client_get_status_code(client),
+                esp_http_client_get_content_length(client));
+    } else {
+        ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
+    }
     esp_http_client_cleanup(client);
 }
 
@@ -521,27 +631,27 @@ static void https_with_url(void)
 }
 #endif // CONFIG_MBEDTLS_CERTIFICATE_BUNDLE
 
-static void https_with_hostname_path(void)
-{
-    esp_http_client_config_t config = {
-        .host = "www.howsmyssl.com",
-        .path = "/",
-        .transport_type = HTTP_TRANSPORT_OVER_SSL,
-        .event_handler = _http_event_handler,
-        .cert_pem = howsmyssl_com_root_cert_pem_start,
-    };
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-    esp_err_t err = esp_http_client_perform(client);
+// static void https_with_hostname_path(void)
+// {
+//     esp_http_client_config_t config = {
+//         .host = "www.howsmyssl.com",
+//         .path = "/",
+//         .transport_type = HTTP_TRANSPORT_OVER_SSL,
+//         .event_handler = _http_event_handler,
+//         .cert_pem = howsmyssl_com_root_cert_pem_start,
+//     };
+//     esp_http_client_handle_t client = esp_http_client_init(&config);
+//     esp_err_t err = esp_http_client_perform(client);
 
-    if (err == ESP_OK) {
-        ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %"PRIu64,
-                esp_http_client_get_status_code(client),
-                esp_http_client_get_content_length(client));
-    } else {
-        ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
-    }
-    esp_http_client_cleanup(client);
-}
+//     if (err == ESP_OK) {
+//         ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %"PRIu64,
+//                 esp_http_client_get_status_code(client),
+//                 esp_http_client_get_content_length(client));
+//     } else {
+//         ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
+//     }
+//     esp_http_client_cleanup(client);
+// }
 
 // static void http_encoded_query(void)
 // {
@@ -629,25 +739,25 @@ static void http_absolute_redirect_manual(void)
     esp_http_client_cleanup(client);
 }
 
-static void http_redirect_to_https(void)
-{
-    esp_http_client_config_t config = {
-        .url = "http://"CONFIG_EXAMPLE_HTTP_ENDPOINT"/redirect-to?url=https://www.howsmyssl.com",
-        .event_handler = _http_event_handler,
-        .cert_pem = howsmyssl_com_root_cert_pem_start,
-    };
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-    esp_err_t err = esp_http_client_perform(client);
+// static void http_redirect_to_https(void)
+// {
+//     esp_http_client_config_t config = {
+//         .url = "http://"CONFIG_EXAMPLE_HTTP_ENDPOINT"/redirect-to?url=https://www.howsmyssl.com",
+//         .event_handler = _http_event_handler,
+//         .cert_pem = howsmyssl_com_root_cert_pem_start,
+//     };
+//     esp_http_client_handle_t client = esp_http_client_init(&config);
+//     esp_err_t err = esp_http_client_perform(client);
 
-    if (err == ESP_OK) {
-        ESP_LOGI(TAG, "HTTP redirect to HTTPS Status = %d, content_length = %"PRIu64,
-                esp_http_client_get_status_code(client),
-                esp_http_client_get_content_length(client));
-    } else {
-        ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
-    }
-    esp_http_client_cleanup(client);
-}
+//     if (err == ESP_OK) {
+//         ESP_LOGI(TAG, "HTTP redirect to HTTPS Status = %d, content_length = %"PRIu64,
+//                 esp_http_client_get_status_code(client),
+//                 esp_http_client_get_content_length(client));
+//     } else {
+//         ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
+//     }
+//     esp_http_client_cleanup(client);
+// }
 
 
 static void http_download_chunk(void)
@@ -704,38 +814,38 @@ static void http_perform_as_stream_reader(void)
     free(buffer);
 }
 
-static void https_async(void)
-{
-    esp_http_client_config_t config = {
-        .url = "https://postman-echo.com/post",
-        .event_handler = _http_event_handler,
-        .cert_pem = postman_root_cert_pem_start,
-        .is_async = true,
-        .timeout_ms = 5000,
-    };
-    esp_http_client_handle_t client = esp_http_client_init(&config);
-    esp_err_t err;
-    const char *post_data = "Using a Palant¨ªr requires a person with great strength of will and wisdom. The Palant¨ªri were meant to "
-                            "be used by the D¨²nedain to communicate throughout the Realms in Exile. During the War of the Ring, "
-                            "the Palant¨ªri were used by many individuals. Sauron used the Ithil-stone to take advantage of the users "
-                            "of the other two stones, the Orthanc-stone and Anor-stone, but was also susceptible to deception himself.";
-    esp_http_client_set_method(client, HTTP_METHOD_POST);
-    esp_http_client_set_post_field(client, post_data, strlen(post_data));
-    while (1) {
-        err = esp_http_client_perform(client);
-        if (err != ESP_ERR_HTTP_EAGAIN) {
-            break;
-        }
-    }
-    if (err == ESP_OK) {
-        ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %"PRIu64,
-                esp_http_client_get_status_code(client),
-                esp_http_client_get_content_length(client));
-    } else {
-        ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
-    }
-    esp_http_client_cleanup(client);
-}
+// static void https_async(void)
+// {
+//     esp_http_client_config_t config = {
+//         .url = "https://postman-echo.com/post",
+//         .event_handler = _http_event_handler,
+//         .cert_pem = postman_root_cert_pem_start,
+//         .is_async = true,
+//         .timeout_ms = 5000,
+//     };
+//     esp_http_client_handle_t client = esp_http_client_init(&config);
+//     esp_err_t err;
+//     const char *post_data = "Using a Palant¨ªr requires a person with great strength of will and wisdom. The Palant¨ªri were meant to "
+//                             "be used by the D¨²nedain to communicate throughout the Realms in Exile. During the War of the Ring, "
+//                             "the Palant¨ªri were used by many individuals. Sauron used the Ithil-stone to take advantage of the users "
+//                             "of the other two stones, the Orthanc-stone and Anor-stone, but was also susceptible to deception himself.";
+//     esp_http_client_set_method(client, HTTP_METHOD_POST);
+//     esp_http_client_set_post_field(client, post_data, strlen(post_data));
+//     while (1) {
+//         err = esp_http_client_perform(client);
+//         if (err != ESP_ERR_HTTP_EAGAIN) {
+//             break;
+//         }
+//     }
+//     if (err == ESP_OK) {
+//         ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %"PRIu64,
+//                 esp_http_client_get_status_code(client),
+//                 esp_http_client_get_content_length(client));
+//     } else {
+//         ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
+//     }
+//     esp_http_client_cleanup(client);
+// }
 
 static void https_with_invalid_url(void)
 {
@@ -877,7 +987,7 @@ static void http_test_task(void *pvParameters)
     while(1)
     {
         // http_rest_with_url();
-        http_get_weather();
+        https_get_weather();
     }
 
 #if 0
